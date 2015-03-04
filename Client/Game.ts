ceaa12659f3679a6b30bcce5c0b97b48c0d7558e
Krzysztof Tryniecki <krzszt@outlook.com>
2015-03-04 21:14:48 +0100

    Animations

--- Client/Game.ts
+++ Client/Game.ts
@@ -3,19 +3,22 @@
 	private ground: Ground;
 	private player: Player;
 	private camera: Camera;
-
+	private animations: AnimationContainer;
 	constructor() {
 		this.camera = new Camera();
 		this.ground = new Ground(this.camera);
 		this.player = new Player();
+		this.animations = new AnimationContainer();
 		this.camera.SetCameraTarget(this.player);
 		Ticker.Add(this.player);
+		Ticker.Add(this.animations);
 	}
 
 	Render() {
 		this.ground.Render();
 		this.player.CheckKeyPress();
 		this.player.Render();
+		this.animations.Render();
 		this.camera.UpdateCamera();
 	}
 }
