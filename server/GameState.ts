dbac7416beec8d9c2ecb3c58ccac6ce7ec9adc8b
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-10-08 11:59:57 -0300

    :white_check_mark: fixes default exports resources configuration

--- server/GameState.ts
+++ server/GameState.ts
@@ -1,6 +1,7 @@
 ﻿import {CharacterList} from "./classes/CharacterList";
 import {Ground} from "./classes/Ground";
+import config from "../resources/data.json";
 
-export {default as config} from "../resources/data.json";
+export {config};
 export const characterList = new CharacterList();
 export const ground = new Ground();
